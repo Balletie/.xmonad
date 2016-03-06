a22c1a764925d2dfd9f8c95656fd9a15c53cc8d5
Balletie <skip_meesie@hotmail.com>
2016-03-06 19:05:44 +0100

    Initial commit

---
+++ xmonad.hs
@@ -0,0 +1,107 @@
+import XMonad
+import qualified XMonad.StackSet as W
+import XMonad.Actions.UpdatePointer
+import XMonad.Actions.Submap
+import XMonad.Actions.WindowGo
+import XMonad.Config.Xfce
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops hiding (fullscreenEventHook)
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.Spacing
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.BoringWindows
+import XMonad.Layout.SubLayouts
+import XMonad.Layout.WindowNavigation
+import XMonad.Util.Themes
+import Data.Monoid
+import Data.Map as M hiding (keys)
+
+main :: IO()
+main = do
+  xmonad myConfig
+
+boilerPlateConfig = xfceConfig
+
+myTheme = theme smallClean
+
+myLayout = avoidStruts $ fullscreenFull $ windowNavigation $ subTabbed $ boringWindows
+         $ smartBorders $ withBorder 2 $ smartSpacingWithEdge 4 $ layout
+  where layout  = tiled ||| Mirror tiled ||| Full
+        -- default tiling algorithm partitions the screen into two panes
+        tiled   = Tall nmaster delta ratio
+        -- The default number of windows in the master pane
+        nmaster = 1
+        -- Default proportion of screen occupied by master pane
+        ratio   = 1/2
+        -- Percent of screen to increment by when resizing panes
+        delta   = 3/100
+
+myLogHook = logHook boilerPlateConfig
+         >> fadeInactiveLogHook opacity
+         >> updatePointer pointerPos (0, 0)
+  where opacity = 0.9
+        pointerPos = (0.5, 0.5)
+
+myEventHook =
+     docksEventHook
+  <> fullscreenEventHook
+  <> handleEventHook boilerPlateConfig
+
+isNotification :: Query Bool
+isNotification = isInProperty "_NET_WM_WINDOW_TYPE" "_NET_WM_WINDOW_TYPE_NOTIFICATION"
+
+myManageHook =
+     manageHook boilerPlateConfig
+  <> fullscreenManageHook
+  <> (className =? "Xfrun4" --> doFloat)
+  -- Merge surf windows with tabbed group
+  -- <> (className =? "Surf" --> )
+  <> (className =? "Firefox" --> doShift "web")
+  <> (className =? "Emacs" --> doShift "write")
+  <> (className =? "Claws-mail" --> doShift "mail-chat")
+  <> (isNotification --> doIgnore)
+
+myStartupHook = do
+  startupHook boilerPlateConfig
+  spawn "compton"
+
+myKeys config@(XConfig { modMask = mod }) = additionalKeys <> defaultKeys
+  where defaultKeys = keys boilerPlateConfig $ config
+        additionalKeys = M.fromList $
+          [ ((noModMask, xK_Print)      , spawn "scrot -e 'mv $f ~/Desktop'")
+          , ((controlMask, xK_F12)      , spawn "xmonad --recompile && xmonad --restart && tput bel")
+
+          -- These use boringWindows to skip over e.g. tabs when switching
+          , ((mod, xK_k)                , focusUp)
+          , ((mod, xK_j)                , focusDown)
+
+          , ((mod, xK_s)                , submap $ defaultSublMap config)
+
+          , ((mod .|. controlMask, xK_h), sendMessage $ pullGroup L)
+          , ((mod .|. controlMask, xK_l), sendMessage $ pullGroup R)
+          , ((mod .|. controlMask, xK_k), sendMessage $ pullGroup U)
+          , ((mod .|. controlMask, xK_j), sendMessage $ pullGroup D)
+
+          , ((mod .|. controlMask, xK_m), withFocused (sendMessage . MergeAll))
+          , ((mod .|. controlMask, xK_u), withFocused (sendMessage . UnMerge))
+          ]
+
+-- Uses the `ewmh` function for adding ewmh functionality
+myConfig = ewmh boilerPlateConfig {
+    workspaces = ["web", "mail-chat", "write", "dev"]
+  , borderWidth = 2
+  , normalBorderColor = "#cccccc"
+  , focusedBorderColor = "#dddddd"
+  , modMask = mod4Mask
+  , keys = myKeys
+  , logHook = myLogHook
+  , handleEventHook = myEventHook
+  , manageHook = myManageHook
+  , startupHook = myStartupHook
+  , layoutHook = myLayout
+  , terminal = "urxvt"
+  }
