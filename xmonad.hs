8105e5c274fd0bd09503e765741bd79f3df31ae4
Balletie <skip_meesie@hotmail.com>
2016-03-18 17:20:32 +0100

    Change layouts

--- xmonad.hs
+++ xmonad.hs
@@ -29,16 +29,18 @@ boilerPlateConfig = desktopConfig
 myTheme = theme smallClean
 
 myLayout = avoidStruts $ fullscreenFull $ windowNavigation $ subTabbed $ boringWindows
-         $ smartBorders $ withBorder 2 $ smartSpacingWithEdge 4 $ layout
-  where layout  = tiled ||| Mirror tiled ||| Full
+         $ modifiedLayout ||| Full
+  where
+        modifiedLayout = smartBorders $ withBorder 2 $ smartSpacingWithEdge 4 $ layout
+        layout         = tiled ||| Mirror tiled
         -- default tiling algorithm partitions the screen into two panes
-        tiled   = Tall nmaster delta ratio
+        tiled          = Tall nmaster delta ratio
         -- The default number of windows in the master pane
-        nmaster = 1
+        nmaster        = 1
         -- Default proportion of screen occupied by master pane
-        ratio   = 1/2
+        ratio          = 1/2
         -- Percent of screen to increment by when resizing panes
-        delta   = 3/100
+        delta          = 3/100
 
 myLogHook = logHook boilerPlateConfig
          >> fadeInactiveLogHook opacity
